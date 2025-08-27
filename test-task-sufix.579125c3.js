0c05c2de7b6e1e22c68ed5e34dad456753663549
TetianaHodis1308 <hodistetianajob@ukr.net>
2025-08-28 01:28:19 +0300

    Updates

---
+++ test-task-sufix.579125c3.js
@@ -0,0 +1,19 @@
+'use strict';
+const tableBtn = document.querySelector('.button-toggle-table');
+const gridBtn = document.querySelector('.button-toggle-grid');
+const tableView = document.querySelector('.table-scroll-container');
+const gridView = document.querySelector('.cards');
+tableBtn.addEventListener('click', ()=>{
+    tableBtn.classList.add('active');
+    gridBtn.classList.remove('active');
+    tableView.style.display = 'block';
+    gridView.style.display = 'none';
+});
+gridBtn.addEventListener('click', ()=>{
+    gridBtn.classList.add('active');
+    tableBtn.classList.remove('active');
+    gridView.style.display = 'grid';
+    tableView.style.display = 'none';
+});
+
+//# sourceMappingURL=test-task-sufix.579125c3.js.map
